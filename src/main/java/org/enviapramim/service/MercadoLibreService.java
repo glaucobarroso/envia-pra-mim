696e1fd2747d30466401b1538ec47b72ac67ef8e
Glauco Junquera <glaucobjunquera@gmail.com>
2017-03-07 10:58:53 -0300

    Adding support for local https

--- src/main/java/org/enviapramim/service/MercadoLibreService.java
+++ src/main/java/org/enviapramim/service/MercadoLibreService.java
@@ -11,8 +11,10 @@ public class MercadoLibreService {
     private Meli meli;
     private static final Long APP_ID = 7164282521643532L;
     private static String APP_SECRET = "Fa4Zcl3Kp41nvNGcyKmFf9jB5Fch1cf3";
-    private static final String AUTH_CODE_CALLBACK = "https://x-pulsar-158711.appspot.com/mlauthcode";
-    private static final String AUTH_CALLBACK = "https://x-pulsar-158711.appspot.com/mlauth";
+    //private static final String AUTH_CODE_CALLBACK = "https://x-pulsar-158711.appspot.com/mlauthcode";
+    //private static final String AUTH_CALLBACK = "https://x-pulsar-158711.appspot.com/mlauth";
+    private static final String AUTH_CODE_CALLBACK = "https://localhost:8443/mlauthcode";
+    private static final String AUTH_CALLBACK = "https://localhost:8443/mlauth";
 
     public MercadoLibreService() {
         meli = new Meli(APP_ID, APP_SECRET);
